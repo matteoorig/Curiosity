dab4b100fa884b5d5c40c3e72674e5b2c45ffaf4
matteoorig <matteoorig@gmail.com>
2024-05-23 17:46:56 +0200

    add fonts

---
+++ app/src/main/java/com/curiosity/common/fonts/FontPoppins.kt
@@ -0,0 +1,26 @@
+package com.curiosity.common.fonts
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.curiosity.R
+
+val Poppins = FontFamily(
+    // Font(R.font.poppins_blackitalic, FontWeight.W900),
+    Font(R.font.poppins_black, FontWeight.W900),
+    // Font(R.font.poppins_extrabolditalic, FontWeight.W800),
+    Font(R.font.poppins_extrabold, FontWeight.W800),
+    // Font(R.font.poppins_bolditalic, FontWeight.W700),
+    Font(R.font.poppins_bold, FontWeight.W700),
+    // Font(R.font.poppins_semibolditalic, FontWeight.W600),
+    Font(R.font.poppins_semibold, FontWeight.W600),
+    //Font(R.font.poppins_mediumitalic, FontWeight.W500),
+    Font(R.font.poppins_medium, FontWeight.W500),
+    Font(R.font.poppins_regular, FontWeight.W400),
+    Font(R.font.poppins_lightitalic, FontWeight.W300),
+    Font(R.font.poppins_light, FontWeight.W300),
+    Font(R.font.poppins_extralightitalic, FontWeight.W200),
+    Font(R.font.poppins_extralight, FontWeight.W200),
+    Font(R.font.poppins_thinitalic, FontWeight.W100),
+    Font(R.font.poppins_thin, FontWeight.W100),
+    )

643583934471b752e90e905b15d5954b20f010eb
matteoorig <matteoorig@gmail.com>
2024-05-23 18:03:34 +0200

    add color palette

--- app/src/main/java/com/curiosity/ui/theme/Color.kt
+++ app/src/main/java/com/curiosity/ui/theme/Color.kt
@@ -9,3 +9,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+// figma palette
+val CuriosityGray = Color(0xFFECECEC)
+val CuriosityViolet = Color(0xFF797B95)
+val CuriosityBlue = Color(0xFF797B95)
+val CuriosityOrange = Color(0xFFF0B94E)
+val CuriosityDarkViolet = Color(0xFF705F9F)
+val CuriosityGreen = Color(0xFF6EAD40)
+val CuriosityPink = Color(0xFFCD4E81)
+val CuriosityYellow = Color(0xFFF0DA4C)

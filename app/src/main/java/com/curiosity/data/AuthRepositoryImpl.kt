92d8fcbd89f1512ecadd57c5a969c5612c1f964e
matteoorig <matteoorig@gmail.com>
2024-05-23 16:23:24 +0200

    add singleton tag to AuthRepositoryImpl

--- app/src/main/java/com/curiosity/data/AuthRepositoryImpl.kt
+++ app/src/main/java/com/curiosity/data/AuthRepositoryImpl.kt
@@ -10,6 +10,7 @@ import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
+import javax.inject.Singleton
 
 /**
  * Implementation of the AuthRepository interface.
@@ -19,12 +20,24 @@ import javax.inject.Inject
  *
  * @param firebaseAuth The FirebaseAuth instance used for authentication operations.
  */
+@Singleton
 open class AuthRepositoryImpl @Inject constructor(
     private val firebaseAuth: FirebaseAuth
 ): AuthRepository {
 
+    /**
+     * Gets the currently authenticated user.
+     *
+     * @return The currently authenticated FirebaseUser, or null if there is no authenticated user.
+     */
     override val currentUser: FirebaseUser? get() = firebaseAuth.currentUser
 
+    /**
+     * Signs out the currently authenticated user.
+     *
+     * This method is a suspend function and should be called from a coroutine or another suspend function.
+     * It performs the sign-out operation on the IO dispatcher to avoid blocking the main thread.
+     */
     override suspend fun signOut() {
         withContext(Dispatchers.IO) {
             firebaseAuth.signOut()

931725b95415a880494bcb36eb942e26815a5574
matteoorig <matteoorig@gmail.com>
2024-06-01 15:51:43 +0200

    add buttons onClick animation

--- app/src/main/java/com/curiosity/common/components/CuriosityDefaultButton.kt
+++ app/src/main/java/com/curiosity/common/components/CuriosityDefaultButton.kt
@@ -5,16 +5,19 @@ package com.curiosity.common.components
  */
 
 import androidx.compose.foundation.border
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
@@ -26,6 +29,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.curiosity.common.fonts.Poppins
 import com.curiosity.ui.theme.CuriosityViolet
+
 
 @Composable
 fun CuriosityDefaultButton(
@@ -57,7 +61,8 @@ fun CuriosityDefaultButton(
                 containerColor =  backgroundColor ?: Color.Transparent
             ),
             shape = RoundedCornerShape(10.dp),
-            onClick = onClick
+            onClick = onClick,
+            interactionSource = remember { MutableInteractionSource() },
         ) {
             Text(
                 text = value,
--- app/src/main/java/com/curiosity/common/components/ProfileButton.kt
+++ app/src/main/java/com/curiosity/common/components/ProfileButton.kt
@@ -7,6 +7,7 @@ package com.curiosity.common.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,9 +17,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -49,10 +52,17 @@ fun ProfileButton(
 ){
     Row(
         modifier = Modifier
-            .clickable { onClick() }
             .width(width ?: 150.dp)
             .height(height ?: 100.dp)
             .clip(RoundedCornerShape(10.dp))
+            .clickable(
+                onClick = onClick,
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberRipple(
+                    color = CuriosityViolet,
+                    bounded = true
+                )
+            )
             .background(CuriosityGray)
             .border(
                 4.dp,

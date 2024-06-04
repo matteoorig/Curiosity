1659d1be215d27555484c877ac85f379bdfa3430
matteoorig <matteoorig@gmail.com>
2024-06-04 12:15:34 +0200

    add hilt-work dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("com.google.guava:guava:31.1-android")
+    implementation("androidx.hilt:hilt-common:1.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -80,6 +83,7 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.49")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
     implementation("com.google.dagger:hilt-android:2.49")
+    implementation("androidx.hilt:hilt-work:1.2.0")
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     // adding compose navigation dependencies from https://developer.android.com/develop/ui/compose/navigation?hl=it

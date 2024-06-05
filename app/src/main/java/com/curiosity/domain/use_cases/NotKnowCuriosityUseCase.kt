97292c41e928a72f61d69270190b616a6a727a58
matteoorig <matteoorig@gmail.com>
2024-06-05 11:19:09 +0200

    add implementation of user actions in home screen

---
+++ app/src/main/java/com/curiosity/domain/use_cases/NotKnowCuriosityUseCase.kt
@@ -0,0 +1,38 @@
+package com.curiosity.domain.use_cases
+
+/**
+ * @author matteooriggi
+ */
+
+import com.curiosity.domain.model.CoinsState
+import com.curiosity.domain.model.Resource
+import com.curiosity.domain.model.User
+import com.curiosity.domain.repository.SharedPreferencesRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class NotKnowCuriosityUseCase @Inject constructor(
+    private val sharedPreferencesRepository: SharedPreferencesRepository
+) {
+    operator fun invoke(user: MutableStateFlow<User>): Flow<Resource<Boolean>> = flow {
+        try {
+
+            emit(Resource.Loading<Boolean>())
+
+            user.value = user.value.copy(
+                coins = user.value.coins + CoinsState.VICTORY.value
+            )
+
+            // Save current user in sharedPreferences
+            sharedPreferencesRepository.saveUser(user.value)
+
+
+        }catch (e: Exception){
+            emit(Resource.Error<Boolean>("NotKnowCuriosityUseCase" + e.message))
+        }
+    }.flowOn(Dispatchers.IO)
+}
